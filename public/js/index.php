f3ba114cf4cf8aeb21f0b7489130d1a44546a712
Bruna <brunaa.ncarvalho17@gmail.com>
2025-12-04 22:34:25 -0300

    Adicionadas imagens e atualizado HTML da loja Flamengo

--- public/js/index.php
+++ public/js/index.php
@@ -1,116 +1,37 @@
-<?php
-session_start();
+<div class="produtos">
+    <div class="produto">
+        <img src="img/bone.png" alt="Boné Flamengo">
+        <h3>Boné Flamengo</h3>
+        <p>R$ 79,90</p>
+    </div>
 
-require __DIR__ . '/../vendor/autoload.php';
+    <div class="produto">
+        <img src="img/camisa.jpeg" alt="Camisa Flamengo">
+        <h3>Camisa Flamengo</h3>
+        <p>R$ 249,90</p>
+    </div>
 
-use App\Controllers\UsuarioController;
-use App\Controllers\ProdutoController;
+    <div class="produto">
+        <img src="img/jaqueta.jpg" alt="Jaqueta Flamengo">
+        <h3>Jaqueta Flamengo</h3>
+        <p>R$ 299,90</p>
+    </div>
 
-function render($view, $data = [])
-{
-    extract($data);
-    ob_start();
-    require __DIR__ . '/../app/Views/' . $view;
-    $content = ob_get_clean();
-    require __DIR__ . '/../app/Views/layouts/base.php';
-}
+    <div class="produto">
+        <img src="img/tenis.png" alt="Tênis Flamengo">
+        <h3>Tênis Flamengo</h3>
+        <p>R$ 199,90</p>
+    </div>
 
-function render_sem_template($view, $data = [])
-{
-    extract($data);
-    require __DIR__ . '/../app/Views/' . $view;
-}
-
-$url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
-$usuarioCtrl = new UsuarioController();
-$produtoCtrl  = new ProdutoController();
-
-/* ------------------------- ROTAS GERAIS ------------------------- */
-
-if ($url === "/" || $url === "/index.php") {
-
-    render_sem_template('home.php', [
-        'title' => 'Bem-vindo!',
-        'lenda' => 'Agora eu sou uma lêndia do PHP!'
-    ]);
-
-} else if ($url === "/sobre") {
-
-    render('sobre.php', ['title' => 'Sobre a Página']);
-
-
-/* ------------------------- ROTAS DE USUÁRIOS ------------------------- */
-
-} else if ($url === "/usuarios") {
-
-    $usuarioCtrl->listar();
-
-} else if ($url === "/usuarios/inserir") {
-
-    render('usuarios/form_usuarios.php', [
-        'title' => 'Cadastrar Usuário!',
-        'dados' => []
-    ]);
-
-} else if ($url === "/usuarios/editar") {
-
-    if (isset($_GET['id'])) {
-        $usuarioCtrl->editar($_GET['id']);
-    } else {
-        header('Location: /usuarios');
-    }
-
-} else if ($url === "/usuarios/excluir") {
-
-    if (isset($_GET['id'])) {
-        $usuarioCtrl->excluir($_GET['id']);
-    } else {
-        header('Location: /usuarios');
-    }
-
-} else if ($url === "/usuarios/salvar" && $_SERVER['REQUEST_METHOD'] === 'POST') {
-
-    $usuarioCtrl->salvar();
-
-
-/* ------------------------- ROTAS DE PRODUTOS ------------------------- */
-
-} else if ($url === "/produtos") {
-
-    $produtoCtrl->listar();
-
-} else if ($url === "/produtos/inserir") {
-
-    render('produtos/form_produtos.php', [
-        'title' => 'Cadastrar Produto!',
-        'dados' => []
-    ]);
-
-} else if ($url === "/produtos/editar") {
-
-    if (isset($_GET['id'])) {
-        $produtoCtrl->editar($_GET['id']);
-    } else {
-        header('Location: /produtos');
-    }
-
-} else if ($url === "/produtos/excluir") {
-
-    if (isset($_GET['id'])) {
-        $produtoCtrl->excluir($_GET['id']);
-    } else {
-        header('Location: /produtos');
-    }
-
-} else if ($url === "/produtos/salvar" && $_SERVER['REQUEST_METHOD'] === 'POST') {
-
-    $produtoCtrl->salvar();
-
-
-/* ------------------------- 404 ------------------------- */
-
-} else {
-
-    echo "<h1>Erro 404 - Página não encontrada.</h1>";
-}
+    <div class="produto">
+        <img src="img/shorts.jpeg" alt="Shorts Flamengo">
+        <h3>Shorts Flamengo</h3>
+        <p>R$ 89,90</p>
+    </div>
+</div>
+    <div class="produto">
+        <img src="img/mochila.png" alt="Mochila Flamengo">
+        <h3>Mochila Flamengo</h3>
+        <p>R$ 149,90</p>
+    </div>
+</div>
